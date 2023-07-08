286c775b3cb2bcd18e86346146fd9b6c3011f929
Илья Иванов <bigfut@list.ru>
2023-07-08 16:44:41 +0300

    change settings

--- corpquiz/settings.py
+++ corpquiz/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g!%))6$!%zn)n0oo!y!(9urimzeu-$o&%pt22lcgg&xf)+9dzq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'localhost', '80.87.106.133']
 
