d4783c6eaaed25aa105900b58d540d1dbe2f8c6e
Илья Иванов <bigfut@list.ru>
2023-07-31 17:44:21 +0300

    add admin app

---
+++ admin/urls.py
@@ -0,0 +1,26 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from quizes.views import (
+    QuestionAdminViewSet,
+    QuizAdminViewSet,
+    QuizLevelViewSet,
+    TagViewSet,
+)
+from user.views import DepartmentViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register('levels', QuizLevelViewSet, basename='quiz-levels')
+router_v1.register('tags', TagViewSet, basename='quiz-tags')
+router_v1.register('quizes', QuizAdminViewSet, basename='quiz-admin')
+router_v1.register(
+    'quizes/questions', QuestionAdminViewSet, basename='quiz-questions'
+)
+router_v1.register(
+    r'quizes/(?P<quiz_id>[\d]+)/questions', QuestionAdminViewSet,
+    basename='quiz-questions-list'
+)
+router_v1.register('departments', DepartmentViewSet)
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]
--- quizes/urls.py
+++ quizes/urls.py
@@ -1,32 +1,16 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import (
-    QuestionAdminViewSet,
-    QuizAdminViewSet,
-    QuizLevelViewSet,
     QuizViewSet,
-    TagViewSet,
     UserQuestionViewSet,
 )
 
 router_v1 = DefaultRouter()
-router_v1.register('admin/levels', QuizLevelViewSet, basename='quiz-levels')
-router_v1.register('admin/tags', TagViewSet, basename='quiz-tags')
-router_v1.register('admin', QuizAdminViewSet, basename='quiz-admin')
-router_v1.register(
-    'admin/quizes/questions', QuestionAdminViewSet, basename='quiz-questions'
-)
 router_v1.register('', QuizViewSet, basename='quizes')
 router_v1.register(
      r'(?P<quiz_id>[\d]+)/answer', UserQuestionViewSet, basename='userquestion'
-)
-router_v1.register(
-    r'admin/quizes/(?P<quiz_id>[\d]+)/questions', QuestionAdminViewSet,
-    basename='quiz-questions-list'
 )
 
 urlpatterns = [  
      path('', include(router_v1.urls)),
 ]
-
-urlpatterns += router_v1.urls
