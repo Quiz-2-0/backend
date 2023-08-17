eaec9c0310d9bcfb1ce3623df502568fa776fabe
Taron <tar8117@yandex.ru>
2023-08-17 20:06:50 +0600

    trying to fix PUT error

--- quizes/views.py
+++ quizes/views.py
@@ -246,16 +246,12 @@ class QuizAdminViewSet(viewsets.ModelViewSet):
             raise ValidationError('Тег с указанным id не существует.')
 
     def perform_update(self, serializer):
+        quiz = serializer.save()
         tags_data = self.request.data.get('tags', [])
-        tag_id = tags_data[0].get('id')
-        try:
-            # Пытаемся найти тег по id
-            tag = models.Tag.objects.get(id=tag_id)
-            quiz = serializer.save()
-            # Добавляем тег к квизу
-            quiz.tags.add(tag)
-        except models.Tag.DoesNotExist:
-            raise ValidationError('Тег с указанным id не существует.')
+        tag_ids = [tag_data.get('id') for tag_data in tags_data]
+
+        quiz.tags.clear()  # Удаление существующих связей с тегами
+        quiz.tags.set(tag_ids)  # Установка новых связей
 
     def perform_destroy(self, instance):
         instance.delete()
