b5b05f2c76a0176de5db45297368de46e2f32a1d
Илья Иванов <bigfut@list.ru>
2023-08-18 15:34:29 +0300

    fix send mail

--- user/utils.py
+++ user/utils.py
@@ -1,11 +1,12 @@
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 def password_mail(email, password):
     send_mail(
             'Авторизационные данные',
             f'Ваш пароль для входа: {password}',
-            'corpquiz@yandex.ru',
-            (email,),
+            settings.EMAIL_HOST_USER,
+            [email],
             fail_silently=False,
         )
