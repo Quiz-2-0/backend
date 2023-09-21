a60c89284d18325379b43b28a914ef3b9163b0ce
1 <margoloko@mail.ru>
2023-09-21 10:15:51 +0700

    chore: Рефакторинг приложения user, добавление докстринг

--- user/admin.py
+++ user/admin.py
@@ -1,24 +1,51 @@
 from django.contrib import admin
-from user.models import User, DefaultAvatar, Department
+
+from user.models import DefaultAvatar, Department, User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'firstName', 'lastName')
-    search_fields = ('email', 'firstName')
+    """
+    Административный класс для модели User.
+
+    Отображает email, имя и фамилию пользователя в списке пользователей.
+    Настроен поиск пользователей по email и имени.
+    """
+
+    list_display = ("email", "firstName", "lastName")
+    search_fields = ("email", "firstName")
 
     def save_model(self, request, obj, form, change):
-        if not obj.password.startswith('pbkdf2_sha256'):
+        """
+        Сохраняет модель пользователя.
+
+        Если пароль пользователя не был зашифрован,
+        то сначала шифрует пароль, а затем сохраняет модель.
+        """
+        if not obj.password.startswith("pbkdf2_sha256"):
             obj.set_password(obj.password)
         super().save_model(request, obj, form, change)
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    """
+    Административный класс для модели Department.
+
+    Отображает название отдела в списке отделов.
+    Позволяет искать отделы по названию.
+    """
+
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(DefaultAvatar)
 class DefaultAvatarAdmin(admin.ModelAdmin):
-    list_display = ('avatar', 'description')
+    """
+    Административный класс для модели DefaultAvatar.
+
+    Отображает аватар и описание в списке аватаров.
+    """
+
+    list_display = ("avatar", "description")
--- user/permission.py
+++ user/permission.py
@@ -2,17 +2,31 @@ from rest_framework import permissions
 
 
 class AdminOrReadOnly(permissions.BasePermission):
+    """
+    Класс разрешений.
+
+    Позволяет только чтение для всех пользователей,
+    но предоставляет полный доступ для администраторов.
+    """
 
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated and request.user.is_admin)
+        """
+        Проверяет, имеет ли пользователь разрешение на выполнение запроса.
+
+        Если запрос является безопасным или пользователь является администратором,
+        то доступ предоставляется.
+        """
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
         )
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (
-                request.user.is_authenticated
-                and request.user.is_admin)
+        """
+        Проверка доступа пользователя для выполнения запроса к конкретному объекту.
+
+        Если запрос является безопасным или пользователь является администратором,
+        то доступ предоставляется.
+        """
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
         )
